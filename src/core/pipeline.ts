f10633025065c5b9b2c12611dba7a35c5822edbc
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2025-06-06 21:04:57 +0300

    pipeline class

---
+++ src/core/pipeline.ts
@@ -0,0 +1,84 @@
+interface PipelineDescriptor {
+    device: GPUDevice;
+    textureFormat: GPUTextureFormat;
+    shaderModule: GPUShaderModule;
+    bindGroupLayout: GPUBindGroupLayout;
+    vertexEntryPoint: string;
+    fragmentEntryPoint: string;
+    primitiveTopology?: GPUPrimitiveTopology;
+    cullMode?: GPUCullMode;
+    targets?: GPUColorTargetState[];
+    vertexBuffers?: GPUVertexBufferLayout[];
+}
+  
+
+class Pipeline {
+
+    device: GPUDevice;
+    textureFormat: GPUTextureFormat;
+    shaderModule: GPUShaderModule;
+    bindGroupLayout: GPUBindGroupLayout;
+    vertexEntryPoint: string;
+    fragmentEntryPoint: string;
+    primitiveTopology: GPUPrimitiveTopology;
+    cullMode: GPUCullMode;
+    targets: GPUColorTargetState[];
+    vertexBuffers: GPUVertexBufferLayout[];
+    pipeline: GPURenderPipeline;
+
+
+    constructor(pipelineDescriptor: PipelineDescriptor) {
+        const {
+          device,
+          textureFormat,
+          shaderModule,
+          bindGroupLayout,
+          vertexEntryPoint = 'vertex',
+          fragmentEntryPoint = 'fragment',
+          primitiveTopology = 'triangle-list',
+          cullMode = 'back',
+          targets,
+          vertexBuffers = []
+        } = pipelineDescriptor;
+      
+        this.device = device;
+        this.textureFormat = textureFormat;
+        this.shaderModule = shaderModule;
+        this.bindGroupLayout = bindGroupLayout;
+        this.vertexEntryPoint = vertexEntryPoint;
+        this.fragmentEntryPoint = fragmentEntryPoint;
+        this.primitiveTopology = primitiveTopology;
+        this.cullMode = cullMode;
+        this.targets = targets || [{
+          format: textureFormat,
+          writeMask: GPUColorWrite.ALL,
+          blend: {
+            color: { operation: 'add', srcFactor: 'src-alpha', dstFactor: 'one-minus-src-alpha' },
+            alpha: { operation: 'add', srcFactor: 'one', dstFactor: 'zero' }
+          }
+        }];
+        this.vertexBuffers = vertexBuffers;
+        
+        this.pipeline = this.createPipeline();
+      }
+
+    createPipeline(): GPURenderPipeline {
+        return this.device.createRenderPipeline({
+            layout: this.device.createPipelineLayout({ bindGroupLayouts: [this.bindGroupLayout]}),
+            vertex: {
+                module: this.shaderModule,
+                entryPoint: this.vertexEntryPoint,
+                buffers: this.vertexBuffers
+            },
+            fragment: {
+                module: this.shaderModule,
+                entryPoint: this.fragmentEntryPoint,
+                targets: this.targets
+            },
+            primitive: {
+                topology: this.primitiveTopology,
+                cullMode: this.cullMode
+            },
+        })
+    }
+}
