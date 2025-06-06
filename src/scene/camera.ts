0850960449565b5687772086d69f956a124806a9
omar-yasser-ayoub <omar.ayoub@student.guc.edu.eg>
2025-06-07 01:18:29 +0300

    add camera

--- src/scene/camera.ts
+++ src/scene/camera.ts
@@ -1,7 +1,7 @@
-import { mat4, type Vec3, type Vec4 } from "wgpu-matrix";
+import { mat4, type Mat4, type Vec3, type Vec4 } from "wgpu-matrix";
 export class Camera {
-    viewMatrix: Vec4 = mat4.create();
-    projectionMatrix: Vec4 = mat4.create();
+    viewMatrix: Mat4 = mat4.create();
+    projectionMatrix: Mat4 = mat4.create();
     
     constructor(fov: number, aspect: number, near: number, far: number) {
         mat4.perspective(fov, aspect, near, far, this.projectionMatrix);
@@ -10,5 +10,11 @@ export class Camera {
     lookAt(eye: Vec3, target: Vec3, up: Vec3) {
         mat4.lookAt(eye, target, up, this.viewMatrix);
     }
-  }
+
+    getViewProjection(): Mat4 { 
+        const vp = mat4.create();
+        mat4.multiply(this.projectionMatrix, this.viewMatrix, vp);
+        return vp;
+    }
+}
   
